ce778e9e005fd43a0804538cd5c9b3494e4d651e
Alen Joe Antony <techjoespace@gmail.com>
2020-01-25 14:51:57 +0530

    [features/essential] Fix - Edit SolvedQuestionStat model class name
    - Renamed model class name from SolvedQuestion to SolvedQuestionStat

--- app/SolvedQuestionStat.php
+++ app/SolvedQuestionStat.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SolvedQuestion extends Model
+class SolvedQuestionStat extends Model
 {
     /**
      * The table associated with the model.
