76d99d543dd4ba2e607faff5d1e9e6d9af430505
Alen Joe Antony <techjoespace@gmail.com>
2020-02-25 06:27:10 +0530

    [master] Fix - Restrict unauthorized access to pages
    - ADD: Restrict registration to logged in users
    - FIX: Restrict game to active in users
    - FIX: Restrict admin pages to logged in admins

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,16 +12,11 @@ class AdminController extends Controller
 {
     function index() {
         $user_type = User::where('username', Auth::user()->username)->first()->user_type;
-        if($user_type == 'admin') {
-            $entry = DB::table('users')
-                ->join('user_levels', 'users.username', '=', 'user_levels.username')
-                ->select('users.name', 'users.username', 'users.user_type', 'users.status', 'user_levels.current_level', 'user_levels.coins')
-                ->get();
-            return view('admin', ['entry' => $entry]);
-        }
-        else {
-            return "<h1>Sorry, you have no clearance !</h1>";
-        }
+        $entry = DB::table('users')
+            ->join('user_levels', 'users.username', '=', 'user_levels.username')
+            ->select('users.name', 'users.username', 'users.user_type', 'users.status', 'user_levels.current_level', 'user_levels.coins')
+            ->get();
+        return view('admin', ['entry' => $entry]);
     }
 
     function viewProfile($username) {
@@ -37,7 +32,6 @@ class AdminController extends Controller
         $coins = $user_level->coins;
         $coins_inc = $request->get('num_coins');
         $user_level->update(array('coins' => $coins + $coins_inc));
-        // AdminController::viewProfile($username);
         return back();
     }
 
--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'SocialLoginController@index');
 Route::get('/login', 'SocialLoginController@index');
-
 Route::get('/auth/redirect/{provider}', 'SocialLoginController@auth_redirect');
 Route::get('/auth/callback/{provider}', 'SocialLoginController@auth_callback');
-Route::post('/auth/register', 'SocialLoginController@registerUser');
-Route::get('/auth/logout', 'SocialLoginController@logout');
 
-Route::group(['middleware' => 'App\Http\Middleware\ActiveUserMiddleware'], function()
-{
+Route::group(['middleware' => 'App\Http\Middleware\CheckLoginMiddleware'], function () {
+    Route::post('/auth/register', 'SocialLoginController@registerUser');
+    Route::get('/auth/logout', 'SocialLoginController@logout');
+});
+
+Route::group(['middleware' => ['App\Http\Middleware\CheckLoginMiddleware', 'App\Http\Middleware\ActiveUserMiddleware']], function() {
     Route::get('/game', 'GameController@index');
     Route::get('/game/question', 'GameController@getQuestion');
     Route::post('/game/submitAnswer', 'GameController@submitAnswer');
     Route::get('/game/leaderboard', 'GameController@leaderboard');
     Route::post('/game/coins', 'GameController@getCoins');
     Route::get('/game/profile', 'GameController@viewProfile');
+});
 
-    Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function()
-    {
-        Route::get('/admin', 'AdminController@index');
-        Route::get('/admin/view/profile/{username}', 'AdminController@viewProfile');
-        Route::post('/admin/actions/coins_giveaway/{username}', 'AdminController@coinsGiveaway');
-        Route::post('/admin/actions/change_user_type/{username}', 'AdminController@changeUserType');
-        Route::get('/admin/actions/block_user/{username}', 'AdminController@blockUser');
-        Route::post('/admin/actions/coins_giveaway_all', 'AdminController@coinsGiveawayAll');
-    });
+Route::group(['middleware' => ['App\Http\Middleware\CheckLoginMiddleware', 'App\Http\Middleware\ActiveUserMiddleware', 'App\Http\Middleware\AdminMiddleware']], function () {
+    Route::get('/admin', 'AdminController@index');
+    Route::get('/admin/view/profile/{username}', 'AdminController@viewProfile');
+    Route::post('/admin/actions/coins_giveaway/{username}', 'AdminController@coinsGiveaway');
+    Route::post('/admin/actions/change_user_type/{username}', 'AdminController@changeUserType');
+    Route::get('/admin/actions/block_user/{username}', 'AdminController@blockUser');
+    Route::post('/admin/actions/coins_giveaway_all', 'AdminController@coinsGiveawayAll');
 });
