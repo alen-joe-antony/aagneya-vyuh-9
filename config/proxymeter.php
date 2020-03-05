4bf616a8fe5d39b216a5f6e5e7c37f16836cfb3c
Alen Joe Antony <techjoespace@gmail.com>
2020-03-05 11:19:47 +0530

    [master] Update Proxymeter entries

--- config/proxymeter.php
+++ config/proxymeter.php
@@ -3,25 +3,218 @@
 return [
     'levels' => [
         1 => [
-            'STADIA',
-            'GOOGLE STADIA',
-            'CLOUD GAMING',
-            'GAMING',
-            'GOOGLE'
+            'JIMMY DONALDSON',
+            'MRBEAST',
+            'YOUTUBER',
+            'YOUTUBE',
+            'TEAM TREES'
         ],
         2 => [
-            'GUINEVERE',
-            'LANCELOT',
-            'KING ARTHUR',
-            'LOVE TRIANGLE',
-            'KNIGHTS OF THE ROUND TABLE'
+            'CENTROLENIDAE',
+            'GLASS FROG',
+            'INVISIBLE FROG'
         ],
         3 => [
-            'ADAMANTIUM',
-            'WOLVERINE',
-            'HEALING',
-            'METAL',
-            'ROD OF ASCLEPIUS'
-        ]
+            'ONEPLUS',
+            'NEVER SETTLE'
+        ],
+		4 => [
+            'THE GOLDEN SNITCH',
+            'GOLDEN SNITCH',
+            'SNITCH',
+            'QUIDDITCH',
+            'HARRY POTTER'
+        ],
+		5 => [
+            'SAMSUNG GALAXY NOTE 7',
+            'GALAXY NOTE 7',
+            'NOTE 7',
+            'SAMSUNG',
+            'PHONE'
+        ],
+		6 => [
+            'BTS',
+            'MAP OF THE SOUL',
+            'PERSONA',
+            'DIONYSUS',
+            'CARL JUNG'
+        ],
+		7 => [
+            'ULTRON',
+            'AVENGERS AGE OF ULTRON',
+            'AVENGERS',
+            'TONY STARK',
+            'IRON MAN'
+        ],
+		8 => [
+            'RADWIMPS',
+            'TENKI NO KO',
+            'CHILD OF WEATHER',
+			'WEATHERING WITH YOU',
+            'SOUNDTRACK',
+            'MUSIC'
+        ],
+		9 => [
+            'NIKOLAI ANDREYEVICH RIMSKI KORSAKOV',
+            'NIKOLAI RIMSKI',
+            'FLIGHT OF THE BUMBLEBEE',
+            'THE FIVE',
+            'RUSSIA'
+        ],
+		10 => [
+            'WILLIAM JAMES MITCHELL JR',
+            'BILLY MITCHELL',
+            '3333360',
+            'PACMAN',
+            'ANGEL NUMBER'
+        ],
+		11 => [
+            'HELLO WORLD',
+            'BRAINFUCK',
+            'BRAINFUCK',
+            'PROGRAMMING'
+        ],
+		12 => [
+            'DISCOVERY INC',
+            'THE DISCOVERY NETWORK',
+            'ANIMAL PLANET',
+            'TLC',
+            'AIRTEL'
+        ],
+		13 => [
+            'LOLCAT',
+            'LOLCODE',
+            'CAT MEMES',
+            'ANIMAL MEMES',
+            'MEMES'
+        ],
+		14 => [
+            'APPLE HOMEPOD',
+            'HOMEPOD',
+            'LASER EXPLOIT',
+            'LASER COMMAND',
+            'LASER'
+        ],
+		15 => [
+            'COMMUNIST PARTY OF INDIA',
+            'COMMUNIST PARTY',
+			'COMMUNIST',
+            'COMMUNISM',
+            'ATHAR HUSSEIN RIZVI',
+            'KAIFI'
+        ],
+		16 => [
+            'BURAN',
+            'BLIZZARD',
+            'SNOWSTORM',
+			'SPACECRAFT',
+			'SOVIET UNION',
+            'RUSSIA',
+            'JYNX'
+        ],
+		17 => [
+            '1031316053',
+            '1009',
+            '1013',
+            'RESOLUTION',
+            'PRIME NUMBERS'
+        ],
+		18 => [
+            'ROLLS ROYCE',
+            'SIR FRANK WHITTLE',
+            'ENGINE'
+        ],
+		19 => [
+            'LOUVRE PYRAMID',
+            'THE LOUVRE PYRAMID',
+            'LOUVRE',
+            'MONA LISA',
+            'VENUS'
+        ],
+		20 => [
+            'STATUE OF UNITY',
+            'SARDAR VALLABHBHAI PATEL',
+            'VALLABHBHAI JHAVERBHAI PATEL',
+            'IRON MAN',
+            'INDIA'
+        ],
+		21 => [
+            'AIR FRANCE FLIGHT 4590',
+            'CONCORDE',
+            'BRITAIN',
+            'FRANCE',
+            'BLACKBOX'
+        ],
+		22 => [
+            'ALASTAIR PRESTON REYNOLDS',
+            'ZIMA BLUE',
+            'LOVE DEATH AND ROBOTS',
+            'FRIENDS',
+            'BOOK'
+        ],
+		23 => [
+            'METAFORMIN AND DEHYDROEPIANDROSTERONE',
+            'DEHYDROEPIANDROSTERONE AND METAFORMIN',
+            'METAFORMIN AND DHEA',
+            'TURRITOPSIS DOHRNII',
+            'JELLYFISH',
+			'FRENCH ACADEMY',
+			'PARIS'
+        ],
+		24 => [
+            'KEYBOARD',
+            'HP OMEN',
+            'DAVE LEE',
+            'PI',
+            'YOUTUBE'
+        ],
+		25 => [
+            'BERGEN PIZZA AND RESTAURANT',
+			'BERGEN STREET',
+            'BROOKLYN NINE NINE',
+            'BROOKLYN 99',
+            'NEW YORK',
+            'STREET VIEW'
+        ],
+		26 => [
+            'CALIXTO CORRIUM',
+            'ELDER SCROLLS V SKYRIM',
+            'BLOOD ON THE ICE',
+            'BLOOD ON ICE',
+            'BLOOD TRAIL',
+			'ICY GRAVE',
+			'MORTAL KOMBAT 11'
+        ],
+		27 => [
+            'LORD WALDER FREY',
+            'DAVID JOHN BRADLEY',
+            'ARGUS FILCH',
+            'HARRY POTTER'
+        ],
+		28 => [
+            'ALL THE LIGHT WE CANNOT SEE',
+            'WERNER',
+            'PARIS LOCK MUSEUM',
+            'MUSEE DE LA SERRURE',
+			'RB LEIPZG',
+            'BUNDESLIGA'
+        ],
+		29 => [
+            'I HAVE FOUND QUEST 30',
+            '432',
+            'HERTZ',
+            'LIFE THE UNIVERSE AND EVERYTHING',
+			'LIFE UNIVERSE AND EVERYTHING',
+            '42'
+        ],
+		30 => [
+            'EDWARD JOSEPH SNOWDEN',
+            'NATIONAL SECURITY AGENCY',
+            'NSA',
+            'ITANIMULLI',
+            'ILLUMINATI',
+			'FREEMASONRY',
+			'BAVARIA'
+        ],
     ]
 ];
