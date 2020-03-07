b76535b679c369b7bd08d91a3c6b8ffd7dea130b
Alen Joe Antony <techjoespace@gmail.com>
2020-03-07 16:45:12 +0530

    [master] Patch 1
    - Change question #24 (image)
    - Additional proxymeter entries

--- config/proxymeter.php
+++ config/proxymeter.php
@@ -12,10 +12,14 @@ return [
         2 => [
             'CENTROLENIDAE',
             'GLASS FROG',
-            'INVISIBLE FROG'
+            'INVISIBLE FROG',
+			'INVISIBLE MAN' 
         ],
         3 => [
             'ONEPLUS',
+			'ONE PLUS',
+			'SMARTPHONE',
+			'PHONE',
             'NEVER SETTLE'
         ],
 		4 => [
@@ -44,34 +48,45 @@ return [
             'AVENGERS AGE OF ULTRON',
             'AVENGERS',
             'TONY STARK',
-            'IRON MAN'
+            'IRON MAN',
+			'RICH'
         ],
 		8 => [
             'RADWIMPS',
             'TENKI NO KO',
             'CHILD OF WEATHER',
-			'WEATHERING WITH YOU',
+			'WEATHERING WITH YOU'
             'SOUNDTRACK',
             'MUSIC'
         ],
 		9 => [
             'NIKOLAI ANDREYEVICH RIMSKY KORSAKOV',
+			'NIKOLAI RIMSKY KORSAKOV',
             'NIKOLAI RIMSKY',
+			'NIKOLAI',
             'FLIGHT OF THE BUMBLEBEE',
             'THE FIVE',
+			'BUMBLEBEE',
+			'BEE',
+			'COMPOSER',
+			'MUSIC',
             'RUSSIA'
         ],
 		10 => [
             'WILLIAM JAMES MITCHELL JR',
             'BILLY MITCHELL',
+			'BILLY',
             '3333360',
+			'HIGH SCORE',
             'PACMAN',
             'ANGEL NUMBER'
         ],
 		11 => [
             'HELLO WORLD',
+            'BRAIN FUCK',
             'BRAINFUCK',
-            'BRAINFUCK',
+			'ESOTERIC PROGRAMMING',
+			'ESOTERIC',
             'PROGRAMMING'
         ],
 		12 => [
@@ -79,11 +94,17 @@ return [
             'THE DISCOVERY NETWORK',
             'ANIMAL PLANET',
             'TLC',
+			'AIRTEL DTH',
+			'AIRTEL DIGITAL TV',
+			'TELEVISION',
+			'TV AD',
+			'ADVERTISEMENT',
             'AIRTEL'
         ],
 		13 => [
             'LOLCAT',
             'LOLCODE',
+			'WEBSITE',
             'CAT MEMES',
             'ANIMAL MEMES',
             'MEMES'
@@ -91,8 +112,16 @@ return [
 		14 => [
             'APPLE HOMEPOD',
             'HOMEPOD',
+			'APPLE',
+			'SIRI',
             'LASER EXPLOIT',
             'LASER COMMAND',
+            'LIGHT EXPLOIT',
+            'LIGHT COMMAND',
+			'LIGHT HACK',
+			'LASER HACK',
+			'HACKER',
+			'HACK',
             'LASER'
         ],
 		15 => [
@@ -100,8 +129,12 @@ return [
             'COMMUNIST PARTY',
 			'COMMUNIST',
             'COMMUNISM',
+			'MARXIST',
+			'MARXISM',
             'ATHAR HUSSEIN RIZVI',
-            'KAIFI'
+            'KAIFI',
+			'ASCII',
+			'BINARY'
         ],
 		16 => [
             'BURAN',
@@ -110,67 +143,131 @@ return [
 			'SPACECRAFT',
 			'SOVIET UNION',
             'RUSSIA',
-            'JYNX'
+            'JYNX',
+			'POKEMON'
         ],
 		17 => [
             '1031316053',
+			'1009X1013',
+			'1013X1009',
             '1009',
             '1013',
             'RESOLUTION',
-            'PRIME NUMBERS'
+            'PRIME NUMBERS',
+			'PRIME',
+			'FACTORIZATION',
+			'PRIME FACTORIZATION'
         ],
 		18 => [
             'ROLLS ROYCE',
             'SIR FRANK WHITTLE',
-            'ENGINE'
+            'ENGINE',
+			'KESTRAL',
+			'PENNINE',
+			'MERLIN',
+			'CRECY',
+			'PLANE',
+			'AIRPLANE',
+			'AEROPLANE'
         ],
 		19 => [
             'LOUVRE PYRAMID',
             'THE LOUVRE PYRAMID',
             'LOUVRE',
+			'THE LOUVRE',
+			'PARIS MUSEUM',
+			'666',
+			'PARIS',
+			'MONA LISA CRATER',
             'MONA LISA',
+			'CRATER',
             'VENUS'
         ],
 		20 => [
             'STATUE OF UNITY',
             'SARDAR VALLABHBHAI PATEL',
             'VALLABHBHAI JHAVERBHAI PATEL',
+			'SARDAR PATEL',
             'IRON MAN',
+			'STATUE',
+			'TONY STARK',
             'INDIA'
         ],
 		21 => [
             'AIR FRANCE FLIGHT 4590',
+			'AIR FRANCE 4590',
+			'4590',
             'CONCORDE',
+			'AEROPLANE',
+			'AIRPLANE',
+			'PLANE',
+			'AIR FRANCE',
+			'PLANE CRASH',
             'BRITAIN',
             'FRANCE',
             'BLACKBOX'
         ],
 		22 => [
             'ALASTAIR PRESTON REYNOLDS',
+			'ALASTIAR REYNOLDS',
+			'ZIMA BLUE AND OTHER STORIES'
             'ZIMA BLUE',
             'LOVE DEATH AND ROBOTS',
+			'EPISODE 14',
+			'AUTHOR',
             'FRIENDS',
+			'NETFLIX',
             'BOOK'
         ],
 		23 => [
             'METAFORMIN AND DEHYDROEPIANDROSTERONE',
             'DEHYDROEPIANDROSTERONE AND METAFORMIN',
             'METAFORMIN AND DHEA',
+			'METAFORMIN',
+			'DEHYDROEPIANDROSTERONE',
+			'DHEA',
+			'DIABETES DRUGS',
+			'DIABETES MEDICINES',
+			'DIABETES DRUG',
+			'DIABETES MEDICINE',
+			'DRUG',
+			'DRUGS',
+			'MEDICINE',
+			'MEDICINE',
+			'NATURE',
+			'EPIGENETIC CLOCK',
             'TURRITOPSIS DOHRNII',
             'JELLYFISH',
+			'COCKTAIL',
+			'REVERSE AGEING',
+			'STOP AGEING',
 			'FRENCH ACADEMY',
+			'GOOGLE MAPS',
 			'PARIS'
         ],
 		24 => [
             'KEYBOARD',
+			'HP OMEN 2018',
+			'OMEN 2018',
+			'HP',
             'HP OMEN',
+			'OMEN'
             'DAVE LEE',
+			'TECH REVIEW',
+			'REVIEW',
+			'3.14',
             'PI',
             'YOUTUBE'
         ],
 		25 => [
             'BERGEN PIZZA AND RESTAURANT',
 			'BERGEN STREET',
+			'BERGEN ST',
+			'POLICE STATION',
+			'78',
+			'GOOGLE MAPS',
+			'2013',
+			'2019',
             'BROOKLYN NINE NINE',
             'BROOKLYN 99',
             'NEW YORK',
@@ -183,38 +280,55 @@ return [
             'BLOOD ON ICE',
             'BLOOD TRAIL',
 			'ICY GRAVE',
+			'SUB ZERO',
+			'SKARLET',
 			'MORTAL KOMBAT 11'
         ],
 		27 => [
             'LORD WALDER FREY',
+			'WALDER FREY',
             'DAVID JOHN BRADLEY',
+			'DAVID BRADLEY',
             'ARGUS FILCH',
+			'GAME OF THRONES',
+			'IRON THRONE',
+			'MRS NORRIS',
             'HARRY POTTER'
         ],
 		28 => [
             'ALL THE LIGHT WE CANNOT SEE',
             'WERNER',
+			'TIMO WERNER'
             'PARIS LOCK MUSEUM',
+			'PARIS MUSUEM',
+			'BOOK',
             'MUSEE DE LA SERRURE',
-			'RB LEIPZG',
+			'RB LEIPZG'
             'BUNDESLIGA'
         ],
 		29 => [
             'I HAVE FOUND QUEST 30',
             '432',
             'HERTZ',
+			'FREQUENCY',
             'LIFE THE UNIVERSE AND EVERYTHING',
 			'LIFE UNIVERSE AND EVERYTHING',
+			'LIFE UNIVERSE EVERYTHING',
             '42'
         ],
 		30 => [
             'EDWARD JOSEPH SNOWDEN',
+			'EDWARD SNOWDEN',
+			'CRYPTOGRAPHY',
             'NATIONAL SECURITY AGENCY',
             'NSA',
             'ITANIMULLI',
+			'MIRROR',
             'ILLUMINATI',
 			'FREEMASONRY',
-			'BAVARIA'
-        ],
+			'FREEMASONS',
+			'BAVARIA',
+			'FREISTAAT BAYERN'
+        ]
     ]
 ];
